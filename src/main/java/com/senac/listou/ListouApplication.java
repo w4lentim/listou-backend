1a0d5e6ab62f1bc4dc30c0f7d87db661160f5623
Willian Valentim <williann.valentim@gmail.com>
2024-01-08 10:08:38 -0300

    Versão e dependências iniciais do projeto

---
+++ src/main/java/com/senac/listou/ListouApplication.java
@@ -0,0 +1,13 @@
+package com.senac.listou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/senac/listou/ListouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.senac.listou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
